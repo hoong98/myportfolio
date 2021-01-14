e423d5d70d33b747a8db14e3245fe3281fa5c0bf
hoong98 <wwhoong98@gmail.com>
2021-01-14 14:46:04 +0800

    new

---
+++ app.js
@@ -0,0 +1,64 @@
+const projectContainer = document.getElementById('project');
+const btns = document.querySelectorAll('.btn');
+const projectEle = document.querySelectorAll('.inner-container');
+const projectTitle = document.querySelector(".project-title");
+const togglerBtn = document.querySelector(".nav-toggle");
+const navContainer = document.querySelector('.nav-container');
+const navItems = document.querySelectorAll(".nav-item");
+const mybutton = document.getElementById("myBtn");
+
+projectContainer.addEventListener('click', function (e) {
+
+    const id = e.target.dataset.id
+    console.log(id);
+    if (id) {
+        btns.forEach(btn => {
+            btn.classList.remove('active');
+
+            e.target.classList.add('active')
+        })
+
+        projectEle.forEach(project => {
+            project.classList.remove('active');
+
+            const innerItem = document.getElementById(id)
+
+            innerItem.classList.add('active')
+        })
+    }
+})
+
+togglerBtn.addEventListener('click', function () {
+    if (!navContainer.classList.contains('show-nav')) {
+        navContainer.classList.add('show-nav')
+    } else {
+        navContainer.classList.remove('show-nav')
+    }
+})
+
+navItems.forEach(navItem => {
+    navItem.addEventListener('click', function () {
+        if (!navContainer.classList.contains('show-nav')) {
+            navContainer.classList.add('show-nav')
+        } else {
+            navContainer.classList.remove('show-nav')
+        }
+    })
+})
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+window.onscroll = function () { scrollFunction() };
